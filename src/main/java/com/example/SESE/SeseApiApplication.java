1941d6e04e58540edd9ecfa997d2583fba675e0d
Moxxy <mikkelinis@gmail.com>
2024-09-11 09:03:03 +0200

    Initial commit og API

---
+++ src/main/java/com/example/SESE/SeseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SESE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeseApiApplication.class, args);
+	}
+
+}
